2e810eb70dfac5fe02e82b820d9f21bc549739b5
mozhants <np4444@yandex.ru>
2020-07-20 22:20:04 +0300

    'upload'

--- app/js/main.js
+++ app/js/main.js
@@ -42,7 +42,11 @@ $(function(){
 			$(this).addClass('active');
 			$('#'+id).addClass('active-tab').fadeIn();
 			return false;
-		});
+	});
+
+	$('input[type="file"], select').styler();
+
+
 
 	
 	$('.menu__btn').on('click', function(){
